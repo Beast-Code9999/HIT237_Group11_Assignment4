f6fb76674ffab5352232377f855ff630788b44a0
Jason_Lay <jason27lay@gmail.com>
2024-04-28 17:05:54 +0930

    removed secret key

--- assignment_4_proj/assignment_4_proj/settings.py
+++ assignment_4_proj/assignment_4_proj/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1g2%+i6bxcv20s5vs$oxyi)a6i9v&=#_pv*wz09gzjk*apl=6i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
