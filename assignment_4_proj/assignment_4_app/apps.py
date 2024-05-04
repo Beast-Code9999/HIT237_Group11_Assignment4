2230e883e07a19249a750753c3accd7f4d1be82a
Jason_Lay <beastcode9999@gmail.com>
2024-05-04 12:20:54 +0930

    removed assignment_2_app and replaced it with assignment_4_app

--- assignment_4_proj/assignment_4_app/apps.py
+++ assignment_4_proj/assignment_4_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Assignment2AppConfig(AppConfig):
+class Assignment4AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'assignment_2_app'
+    name = 'assignment_4_app'
