0a108bf261f61ffcd921d014efe307dbc411c6ba
Jason_Lay <beastcode9999@gmail.com>
2024-05-14 09:48:29 +0930

    add css and urls

--- assignment_4_proj/assignment_4_app/urls.py
+++ assignment_4_proj/assignment_4_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    
+    # http://127.0.0.1:8000/project-list
+    path("project-list/", views.project_list, name="project-list"),
+    # http://127.0.0.1:8000/project-details/<identifier>
+    path("project-details/<slug:slug>/", views.project_details, name="project-details"), # project-details/proj1 etc... using path transformer other e.g. is int or str
+    # http://127.0.0.1:8000/about
+    path("about-us/", views.about, name="about"),   
 ]
--- assignment_4_proj/assignment_4_app/views.py
+++ assignment_4_proj/assignment_4_app/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 def home(request):
     return render(request, "assignment_4_app/index.html")
+
+def project_details(request, slug):
+    try: 
+        return render(request, "assignment2_app/projectDetails.html")
+    except:
+        return HttpResponseNotFound("This link is not supported")
+
+# http://127.0.0.1:8000/project-list
+def project_list(request):
+    try: 
+        return render(request, "assignment2_app/projectList.html")
+    except:     
+        return HttpResponseNotFound("This link is not supported")
+
+
+# http://127.0.0.1:8000/about
+def about(request):
+    try:
+        return render(request, "assignment2_app/about.html")
+    except:
+        return HttpResponseNotFound("This link is not supported")
