b07378c12ebc68b9c1e1df59df0852476d3dbc0c
Jason_Lay <beastcode9999@gmail.com>
2024-05-17 01:05:45 +0930

    add django models template classes

--- assignment_4_proj/assignment_4_app/models.py
+++ assignment_4_proj/assignment_4_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    supvervisor = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+
+class ProjectDetails(models.Model):
+    pass
+
+class Students(models.Model):
+    pass
+
+class Supervisors(models.Model):
+    pass
